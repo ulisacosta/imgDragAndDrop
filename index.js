995744abe8044971556f3ad59051294ed6d17393
unknown <ulisacosta@gmail.com>
2023-06-28 19:57:19 -0300

    Agrego dinamismo al poner el archivo en el area para hacer drop

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-
+const dropArea = document.getElementById('dropArea')
+const dragText = document.querySelector('.dragText')
 const buttonFile = document.getElementById('buttonFile');
 const inputFile = document.getElementById('inputFile');
+
+dropArea.addEventListener('dragover', (e) =>{
+    e.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Drop file"
+})
 
 buttonFile.addEventListener('click', ()=>{
     inputFile.click();
 })
+
