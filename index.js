e0c67bf19f61a2034ffb9e2fc90b792e4d95e92f
unknown <ulisacosta@gmail.com>
2023-07-15 10:25:32 -0300

    agrego server.js .env y .gitignore

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const dropArea = document.getElementById('dropArea')
-const dragText = document.querySelector('.dragText')
+const dropArea = document.getElementById('dropArea');
+const dragText = document.querySelector('.dragText');
 const buttonFile = document.getElementById('buttonFile');
 const inputFile = document.getElementById('inputFile');
 
-dropArea.addEventListener('dragover', (e) =>{
+const ResetTextAreaDrop = "Drag & drop your image here";
+
+dropArea.addEventListener('dragover', (e) => {
     e.preventDefault();
     dropArea.classList.add("active");
-    dragText.textContent = "Drop file"
+    dragText.textContent = "Drop file";
 })
 
-buttonFile.addEventListener('click', ()=>{
-    inputFile.click();
+buttonFile.addEventListener('click', () => inputFile.click());
+
+
+dropArea.addEventListener('drop', (e) => {
+    e.preventDefault();
+
+
+    dragText.textContent = ResetTextAreaDrop;
 })
 
+dropArea.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+
+    dragText.textContent = ResetTextAreaDrop;
+})
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+const port = process.env.PORT|| 3000 ;
+
+app.set('port', port);
+
+app.listen(app.get('port'), () =>{ 
+    console.log('server on port ' + app.get('port') )
+})
