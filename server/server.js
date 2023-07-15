fadbaf094e522f8548405800909342caf1e272eb
unknown <ulisacosta@gmail.com>
2023-07-15 12:08:42 -0300

    server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
+const env = require('../.env')
 const express = require('express')
 
 const app = express();
 
-const port = process.env.PORT  ;
+const port = process.env.PORT || 3000;
 
 app.set('port', port);
 
 app.listen(app.get('port'), () =>{ 
     console.log('server on port ' + app.get('port') )
 })
+
+
+module.exports = app ;
