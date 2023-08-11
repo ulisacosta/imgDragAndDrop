d045f70066c2cafad8e4713fc2a8d31ae9d92b68
ulises <ulisacost@gmail.com>
2023-08-10 21:41:19 -0300

    agrego carpeta public

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para manejar todas las solicitudes, devolviendo el archivo index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
 app.set('port', port);
