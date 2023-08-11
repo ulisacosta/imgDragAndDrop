414e4fa60edfd90f865daebf94e22d11477850eb
ulises <ulisacost@gmail.com>
2023-08-10 21:56:13 -0300

    elimino path.join

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join('index.html'));
+    res.sendFile('index.html');
   });
 
   app.post("/upload",(req,res)=>{
